ff8d4330aaddcc86a0993657a3fccbc752d2d545
Altro <albe020531@outlook.com>
2023-12-03 01:03:23 -0500

    Added PrismaModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,12 @@ import {RewardModule} from './reward/reward.module';
 import {AsistanceModule} from './asistance/asistance.module';
 import {InscriptionModule} from './inscription/inscription.module';
 import {UserModule} from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
     imports: [ConfigModule.forRoot({
-        envFilePath: '.env.local'
-    }), EventModule, UserModule, InscriptionModule, AsistanceModule, RewardModule],
+        envFilePath: '.env'
+    }), EventModule, UserModule, InscriptionModule, AsistanceModule, RewardModule, PrismaModule],
     controllers: [],
     providers: [],
 })
